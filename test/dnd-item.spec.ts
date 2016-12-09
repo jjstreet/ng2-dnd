0a1c901dce99ca66263822fe3961b65a80785e4d
Josh Street <jstree20@ford.com>
2016-12-09 14:55:55 -0500

    modified tsconfig to work with editors; moved build tsconfig to its own file and updated package.json accordingly

--- test/dnd-item.spec.ts
+++ test/dnd-item.spec.ts
@@ -1,5 +1,6 @@
 import {
-	Component
+	Component,
+	ViewChild
 } from '@angular/core';
 
 import {
@@ -8,15 +9,15 @@ import {
 	TestBed
 } from '@angular/core/testing';
 
-import { DndItem } from 'src/dnd-item';
-import { DndModule } from 'src/dnd.module';
+import { DndItem } from '../src/dnd-item';
+import { DndModule } from '../src/dnd.module';
 
 @Component({
 	selector: 'test-cmp',
-	template: '<div dndItem></div>'
+	template: '<div [dndItem]="item"></div>'
 })
 export class TestComponent {
-
+	item: any;
 }
 
 describe('DndItem', () => {
@@ -41,5 +42,16 @@ describe('DndItem', () => {
 		fixture = TestBed.createComponent(TestComponent);
 
 		expect(fixture.componentInstance).toBeDefined();
+	});
+
+	it('should have an assignable item', () => {
+		const item = 'the item';
+
+		fixture = TestBed.createComponent(TestComponent);
+		fixture.componentInstance.item = item;
+
+		fixture.detectChanges();
+
+
 	});
 });
