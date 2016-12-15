14f1650f0fc974a537f910b9b8b33ab595124ea4
Josh Street <josh_s@ameritech.net>
2016-12-15 17:58:09 -0500

    undo event binding updates to dnd item

--- src/dnd-item.ts
+++ src/dnd-item.ts
@@ -16,7 +16,7 @@ import { Point } from './shared';
 @Directive({
 	selector: '[dndItem]'
 })
-export class DndItem implements OnInit, OnDestroy {
+export class DndItem implements OnDestroy {
 
 	@Input() dndItem: any;
 	@Input() dndTargets: string[] = [];
@@ -45,10 +45,6 @@ export class DndItem implements OnInit, OnDestroy {
 
 	private clickPosition: Point;
 
-	private mouseDownListener = this.onMouseDown.bind(this);
-	private mouseMoveListener = this.onMouseMove.bind(this);
-	private mouseUpListener = this.onMouseUp.bind(this);
-
 	private unbindMouseDown: Function;
 	private unbindMouseMove: Function;
 	private unbindMouseUp: Function;
@@ -61,10 +57,7 @@ export class DndItem implements OnInit, OnDestroy {
 			private dnd: DndService,
 			private dndContainer: DndContainer) {
 		this.el = this.elementRef.nativeElement;
-	}
-	
-	ngOnInit() {
-		this.unbindMouseDown = this.renderer.listen(this.el, 'mousedown', this.mouseDownListener);
+		this.unbindMouseDown = this.renderer.listen(this.el, 'mousedown', this.onMouseDown.bind(this));
 	}
 
 	ngOnDestroy() {
@@ -146,8 +139,8 @@ export class DndItem implements OnInit, OnDestroy {
 	}
 
 	private attachDragListeners(): void {
-		this.unbindMouseMove = this.renderer.listenGlobal('document', 'mousemove', this.mouseMoveListener);
-		this.unbindMouseUp = this.renderer.listenGlobal('document', 'mouseup', this.mouseUpListener);
+		this.unbindMouseMove = this.renderer.listenGlobal('document', 'mousemove', this.onMouseMove.bind(this));
+		this.unbindMouseUp = this.renderer.listenGlobal('document', 'mouseup', this.onMouseUp.bind(this));
 	}
 
 	private detachDragListeners(): void {
